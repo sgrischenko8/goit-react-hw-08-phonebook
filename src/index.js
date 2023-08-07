6699ef7f0b0e59616d12ce56cfa314258232795a
sgrischenko8 <120989472+sgrischenko8@users.noreply.github.com>
2023-08-07 11:08:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+  <App />
+</BrowserRouter>
   </React.StrictMode>
 );
